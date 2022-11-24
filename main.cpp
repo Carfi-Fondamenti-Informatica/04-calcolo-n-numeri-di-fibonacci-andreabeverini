07aa83888010f9837564ef7765393b6c1c38281a
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-11-24 20:35:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   int n=0;
+   int a=1;
+   int b=1;
+   int c=0;
+   cin>>n;
+   if (n>=2) {
+      cout<<a<<endl;
+      cout<<b<<endl;
+      for (int i=2; i<n; i++) {
+         c=a+b;
+         cout<<c<<endl;
+         a=b;
+         b=c;
+   }
+   }
+   else {
+       cout<<"errore"<<endl;
+      }
    return 0;
 }
+
